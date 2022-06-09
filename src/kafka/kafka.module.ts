542ff897bf73f070d43937ea177d3d66f0588848
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-06-09 06:50:55 +0100

    Add Consumer

--- src/kafka/kafka.module.ts
+++ src/kafka/kafka.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import {ProducerService} from "./producer.service";
+import {ConsumerService} from "./consumer.service";
 
 @Module({
-    providers: [ProducerService],
-    exports: [ProducerService]
+    providers: [ProducerService,ConsumerService],
+    exports: [ProducerService, ConsumerService]
 })
 export class KafkaModule {}
